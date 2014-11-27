c38a4bdfb0e73827ef0bab7a106181e4ad78ef46
coldrock <william.e.constable@gmail.com>
2014-11-26 18:37:05 -0800

    Readded: Step

---
+++ src/com/zarkoix/ascfiighter/tick/step/Step.java
@@ -0,0 +1,7 @@
+package com.zarkoix.ascfiighter.tick.step;
+
+import com.zarkoix.ascfiighter.tile.entity.player.EntityPlayer;
+
+public interface Step{	
+	public boolean run(EntityPlayer player);
+}
